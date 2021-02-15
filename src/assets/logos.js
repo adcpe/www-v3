c73dba34272a6e7978d8ad78504b8505a9b3ebb9
Andrés Del Carpio <andresdc@andresdc.net>
2021-02-15 17:38:49 -0500

    Added Logos component

---
+++ src/assets/logos.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: 'Ruby',
+    img: require('@/assets/images/ruby.svg'),
+  },
+  {
+    title: 'Ruby on Rails',
+    img: require('@/assets/images/rails.svg'),
+  },
+  {
+    title: 'JavaScript',
+    img: require('@/assets/images/javascript.svg'),
+  },
+  {
+    title: 'Node.js',
+    img: require('@/assets/images/nodejs.svg'),
+  },
+  {
+    title: 'Vue.js',
+    img: require('@/assets/images/vuejs.svg'),
+  },
+  {
+    title: 'React',
+    img: require('@/assets/images/react.svg'),
+  },
+  {
+    title: 'PostgreSQL',
+    img: require('@/assets/images/postgresql.svg'),
+  },
+];
---
+++ src/components/Logos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-row flex-wrap justify-center my-8">
+    <img
+      v-for="(logo, i) in logos"
+      :key="i"
+      :src="logo.img"
+      :alt="logo.title"
+      :title="logo.title"
+      class="h-10 m-4"
+    />
+  </div>
+</template>
+
+<script>
+import logos from '@/assets/logos.js';
+
+export default {
+  data() {
+    return {
+      logos: logos,
+    };
+  },
+};
+</script>
