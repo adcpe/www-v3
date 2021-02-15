33a955c29ee0e397f61d243d2ba60f1d50171edb
Andrés Del Carpio <andresdc@andresdc.net>
2021-02-15 17:39:52 -0500

    Added Home page content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+  <div
+    class="container-sm min-h-full font-lato flex-1 flex flex-col place-content-center items-center mx-10"
+  >
+    <div class="lg:w-1/2">
+      <h1 class="text-3xl m-3">
+        I'm
+        <span class="font-titillium font-bold text-name">
+          Andrés Del Carpio
+        </span>
+      </h1>
+      <p class="text-xl m-3">
+        Full-stack developer, student &#38; technical writer.
+      </p>
+    </div>
+    <Logos />
+  </div>
 </template>
 
 <script>
+import Logos from '../components/Logos';
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    Logos,
+  },
+  created() {
+    document.title = process.env.VUE_APP_TITLE;
+  },
 };
 </script>
